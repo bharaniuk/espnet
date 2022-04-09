43fd5fab45264deffde8346528d996181c69a1e5
ukbharani <ukbharani@gmail.com>
2022-04-09 11:39:50 -0400

    Modified data download to include tamil and malayalam

--- egs2/ml-ta/asr1/local/data.sh
+++ egs2/ml-ta/asr1/local/data.sh
@@ -51,6 +51,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     wget https://www.openslr.org/resources/65/ta_in_male.zip
     unzip -o ta_in_male.zip
     rm -f ta_in_male.zip
+    rm -f tag_01424_01843292347.wav
 
     wget https://www.openslr.org/resources/63/line_index_female.tsv
     wget https://www.openslr.org/resources/63/line_index_male.tsv
@@ -59,6 +60,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     rm -f line_index_male.tsv
     wget https://www.openslr.org/resources/65/line_index_female.tsv
     wget https://www.openslr.org/resources/65/line_index_male.tsv
+    sed -i '/tag_01424_01843292347/d' ./line_index_male.tsv
     cat line_index_female.tsv line_index_male.tsv >> line_index_all.tsv
     cd $workspace
 fi
